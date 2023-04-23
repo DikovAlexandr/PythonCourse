33c772eeb61125958eda14023eceb52c225a3fe4
Диков Александр <112902848+DikovAlexandr@users.noreply.github.com>
2023-04-23 12:41:27 +0300

    FEM problem solved

--- Зачетные задания/Additional chapters of mathematical physics/FEM wave equation/main.cpp
+++ Зачетные задания/Additional chapters of mathematical physics/FEM wave equation/main.cpp
@@ -2,63 +2,70 @@
 #include <fstream>
 #include <iomanip>
 #include <vector>
-#include <cmath>
+#include <math.h>
 
+// Параметры задачи
+const double L = 1, T = 1; // пределы по x и t
+const double H = 0.05, TAU = 0.05; // шаги по пространству и по времени
+const double A = 0.5;
+const int SIZE_X = int(L / H) + 1, SIZE_T = int(T / TAU) + 1;
+
+// начальное условие на координату
 double phi(double x) {
-    return 5.0 / 2 * tan(x);
+    return 5.0 / 2 * tanh(x);
 }
 
+// начальное условие на импульс
 double psi(double x) {
-    return -5 / (2 * pow(cosh(x), 2));
+    return -5.0 / (2 * pow(cosh(x), 2));
 }
 
+// граничное условие в нуле
 double gamma0(double t) {
-    return -5 * tan(t) - 5 / (2 * pow(cosh(t), 2));
+    return -5 * tanh(t) - 5 / (2 * pow(cosh(t), 2));
 }
 
+// граничное условие в L
 double gammaL(double t) {
     return 5 / (2 * pow(cosh(1 - t), 2));
 }
 
+// неоднородность уравнения
 double f(double x, double t) {
-    return 5 * tanh(t - x) / pow(cosh(t - x), 2);
+    return 5.0 / 2 * tanh(t - x) / pow(cosh(t - x), 2);
+}
+
+// подстановка начальных условий
+void initial(std::vector<std::vector<double>> &u) {
+    for (int i = 0; i < SIZE_X; ++i) {
+        u[0][i] = phi(i * H);
+        u[1][i] = u[0][i] + TAU * psi(i * H);
+    }
 }
 
 int main() {
-    double L = 1, T = 1; // пределы по x и t
-    double h = 0.05, tau = 0.05; // шаги по пространству и по времени
-    double a = 0.5;
-    std::vector<std::vector<double>> u(int(L / h) + 1, std::vector<double> (int(T / tau) + 1, 0));
+    std::vector<std::vector<double>> u(SIZE_X, std::vector<double>(SIZE_T, 0));
+    initial(u);
+    for (int n = 2; n < SIZE_T; n++) {
+        for (int i = 1; i < SIZE_X; i++) {
+            if (i != 0 and i * H != L and n > 1)
+                // шаг схемой крест
+                u[n][i] = 2 * u[n - 1][i] - u[n - 2][i] +
+                          pow(A * TAU / H, 2) * (u[n - 1][i + 1] - 2 * u[n - 1][i] + u[n - 1][i - 1]) +
+                          pow(TAU, 2) * f(i * H, (n - 1) * TAU);
 
-    for (int n = 0; n * tau <= T; n++) {
-        for (int i = 0; i * h <= L; i++) {
-            //std::cout << i << ' ' << n << std::endl;
-            if (n == 0)
-                u[i][n] = phi(i * h);
-            if (n == 1)
-                u[i][n] = u[i][0] + tau * psi(i * h);
-            if (i != 0 and i * h != L and n * tau < T and n > 1)
-                u[i][n + 1] =
-                        2 * u[i][n] - u[i][n - 1] + pow(a * tau / h, 2) * (u[i + 1][n] - 2 * u[i][n] + u[i - 1][n]) +
-                        pow(tau, 2) * f(i * h, n * tau);
         }
+        // подстановка граничных условий
+        u[n][0] = (u[n][1] / H + gamma0(n * TAU)) / (2 + 1 / H);
+        u[n][SIZE_X - 1] = (u[n][SIZE_X - 2] / H + gammaL(n * TAU)) / (1 / H);
     }
 
-    for (int n = 0; n * tau <= T; n++) {
-        for (int i = 0; i * h <= L; i++) {
-            if (i == 0)
-                u[i][n] = (u[i + 1][n] + gamma0(n * tau)) / (2 - 1 / h);
-            if (i == L)
-                u[i][n] = (u[i - 1][n] + gammaL(n * tau)) / (1 / h);
-        }
-    }
-
+    // вывод в файл
     std::ofstream fout;
     fout.open(R"(..\\..\\FEM wave equation\\solve.csv)");
-
-    for (auto &i: u) {
-        for (double j: i) {
-            fout << std::setprecision(3) << j << " ";
+    for (auto &t: u) {
+        for (double x: t) {
+            fout << std::setprecision(8) << x << " ";
         }
         fout << std::endl;
     }
